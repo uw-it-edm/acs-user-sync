697069049e9e19187706477de26250149ccd180d
Maxime Deravet <maximed@uw.edu>
2018-02-07 13:36:21 -0800

    CAB-2773 wip

---
+++ src/create-user/model/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    constructor() {
+    }
+
+    userName: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    authorizationStatus= 'AUTHORIZED'
+
+}
