c07460d5cfe08f6ce813aad81929687f275f9589
Andrew Gagne <agagne@uw.edu>
2018-05-02 13:34:25 -0700

    - update webpack to specify mode = production - update webpack to rename loaders to rules for webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,26 @@ const path = require('path');
 const slsw = require('serverless-webpack');
 
 module.exports = {
-  entry: slsw.lib.entries,
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsx',
-      '.json',
-      '.ts',
-      '.tsx'
-    ]
-  },
-  output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
-  },
-  target: 'node',
-  module: {
-    loaders: [
-      { test: /\.ts(x?)$/, loader: 'ts-loader' },
-    ],
-  },
+    entry: slsw.lib.entries,
+    mode: 'production',
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx',
+            '.json',
+            '.ts',
+            '.tsx'
+        ]
+    },
+    output: {
+        libraryTarget: 'commonjs',
+        path: path.join(__dirname, '.webpack'),
+        filename: '[name].js',
+    },
+    target: 'node',
+    module: {
+        rules: [
+            {test: /\.ts(x?)$/, loader: 'ts-loader'},
+        ],
+    },
 };
